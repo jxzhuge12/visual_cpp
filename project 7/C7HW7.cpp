01e796b17cb19b2f962488a9a1b4ded957b9d079
jxzhuge12 <jxzhuge12@gmail.com>
2013-11-18 18:29:29 +0800

    add project 7

---
+++ project 7/C7HW7.cpp
@@ -0,0 +1,68 @@
+#include<windows.h>
+#include<tchar.h>
+
+BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	MSG msg;
+	if (!InitWindowClass(hInstance, nCmdShow)){
+		MessageBox(NULL, L"创建窗口失败！", _T("创建窗口"), NULL);
+		return 1;
+	}
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+	return 0;
+}
+
+BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow){
+	WNDCLASSEX wcex;
+	HWND hWnd;
+	TCHAR szWindowClass [] = L"窗口示例";
+	TCHAR szTitle [] = L"IDR_MENU1";
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = 0;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName = L"MENU";
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	if (!RegisterClassEx(&wcex))
+		return FALSE;
+	hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+	if (!hWnd)
+		return FALSE;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	return TRUE;
+}
